dc699142a8d6ca9099e19dc77232d62fa0a14d7b
Mark Helt <heltmm@gmail.com>
2017-12-18 09:40:46 -0800

    add routes for controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
 
   patch "cellar_update", to: "tanks#cellar_update"
   patch "brewer_update", to: "tanks#brewer_update"
+  patch "transfer_update", to: "tanks#transfer_update"
+  patch "package_update", to: "tanks#package_update"
+  patch "overide", to: "tanks#overide"
+  patch "acid_update", to: "tanks#acid_update"
 
   resources :tanks
 
